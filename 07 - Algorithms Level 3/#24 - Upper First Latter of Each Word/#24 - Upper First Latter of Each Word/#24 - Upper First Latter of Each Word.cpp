16f6187967e03ec39eff62eab38539bfda663ae3
oday <oday.sheikh.dev@gmail.com>
2025-10-22 10:20:26 +0300

    Problems 23 - 29 Added

---
+++ 07 - Algorithms Level 3/#24 - Upper First Latter of Each Word/#24 - Upper First Latter of Each Word/#24 - Upper First Latter of Each Word.cpp
@@ -0,0 +1,79 @@
+/*#include <string>
+#include <iostream>
+using namespace std;
+
+string ReadString()
+{
+    string S1;
+    cout << "Please Enter Your String?\n";
+    getline(cin, S1);
+    return S1;
+}
+
+void PrintUpperFirstLatterOfEachWord(string S1)
+{
+    string LatterUpper = "";
+    string S2 = "";
+    bool isFirstLetter = true;
+    cout << "\nFirst letters of this string:\n";
+
+    for (short i = 0; i < S1.length(); i++)
+    {
+        if (S1[i] != ' ' && isFirstLetter)
+        {
+            S2 += toupper(S1[i]);
+        }
+        else
+        {
+            S2 += S1[i];
+        }
+
+        isFirstLetter = (S1[i] == ' ' ? true : false);
+    }
+    cout << S2 << endl;
+}
+
+int main()
+{
+    PrintUpperFirstLatterOfEachWord(ReadString());
+    system("pause>0");
+}*/
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+string ReadString()
+{
+    string S1;
+    cout << "Please Enter Your String?\n";
+    getline(cin, S1);
+    return S1;
+}
+
+string UpperFirstLetterOfEachWord(string S1)
+{
+    bool isFirstLetter = true;
+
+    for (short i = 0; i < S1.length(); i++)
+    {
+        if (S1[i] != ' ' && isFirstLetter)
+        {
+            S1[i] = toupper(S1[i]);
+        }
+
+        isFirstLetter = (S1[i] == ' ' ? true : false);
+    }
+
+    return S1;
+}
+
+int main()
+{
+    string S1 = ReadString();
+    cout << "\nString after conversion:\n";
+    S1 = UpperFirstLetterOfEachWord(S1);
+    cout << S1 << endl;
+    system("pause>0");
+}
+
---
+++ 07 - Algorithms Level 3/#26 - Upper Lower All Latter of a String/#26 - Upper Lower All Latter of a String/#26 - Upper Lower All Latter of a String.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+string ReadString()
+{
+    string S1;
+    cout << "Please Enter Your String?\n";
+    getline(cin, S1);
+    return S1;
+}
+
+string UpperAllString(string S1)
+{
+    for (short i = 0; i < S1.length(); i++)
+    {
+        S1[i] = toupper(S1[i]);
+
+        /*if (S1[i] != ' ')
+        {
+            S1[i] = toupper(S1[i]);
+        }
+        ãÇÝí ÏÇÚí ÍØ ÔÑØ ááÝÑÇÛ åæ áÍÇáæ ÈíÚÇáÌæ ÓæÇÁ ÈÇáÊßÈíÑ Çæ ÇáÊÕíÛíÑ
+        ÎáÕ ÇäÊ ÈÊßÈÑ ÇáÓáÓáÉ ßÇãáÉ æåæ áÍÇáæ ÈÚÇáÌ ÇáÝÑÇÛÇÊ
+        */
+    }
+
+    return S1;
+}
+
+string LowerAllString(string S1)
+{
+    for (short i = 0; i < S1.length(); i++)
+    {
+        S1[i] = tolower(S1[i]);
+    }
+
+    return S1;
+}
+
+int main()
+{
+    string S1 = ReadString();
+
+    cout << "\nString after Upper:\n";
+    S1 = UpperAllString(S1);
+    cout << S1 << endl;
+
+    cout << "\nString after Lower:\n";
+    S1 = LowerAllString(S1);
+    cout << S1 << endl;
+
+    system("pause>0");
+}
