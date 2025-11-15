33fed19e3f3a7c3e987fa10d588ad9ed0e91e726
oday <oday.sheikh.dev@gmail.com>
2025-11-15 20:33:17 +0300

    Problem 16 Added

---
+++ 08 - Algorithms Level 4/#16 - Incraease Date By One Day/#16 - Incraease Date By One Day/#16 - Incraease Date By One Day.cpp
@@ -0,0 +1,109 @@
+#include <iostream>
+using namespace std;
+
+struct stDate
+{
+    short Year;
+    short Month;
+    short Day;
+};
+
+bool isLeapYear(short Year)
+{
+    return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+}
+
+short NumberOfDaysInAMonth(short Month, short Year)
+{
+    if (Month < 1 || Month > 12)
+        return 0;
+
+    int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+    if (Month == 2)
+        return isLeapYear(Year) ? 29 : 28;
+
+    return days[Month - 1];
+}
+
+bool IsLastDayInMonth(stDate Date)
+{
+    return (Date.Day == NumberOfDaysInAMonth(Date.Month, Date.Year));
+}
+
+bool IsLastMonthInYear(short Month)
+{
+    return (Month == 12);
+}
+
+stDate IncreaseDateByOneDay(stDate Date)
+{
+    if (IsLastDayInMonth(Date))
+    {
+        if (IsLastMonthInYear(Date.Month))
+        {
+            Date.Month = 1;
+            Date.Day = 1;
+            Date.Year++;
+        }
+        else
+        {
+            Date.Day = 1;
+            Date.Month++;
+        }
+    }
+    else
+    {
+        Date.Day++;
+    }
+
+    return Date;
+}
+
+short ReadDay()
+{
+    short Day;
+    cout << "\nPlease enter a Day? ";
+    cin >> Day;
+    return Day;
+}
+
+short ReadMonth()
+{
+    short Month;
+    cout << "Please enter a Month? ";
+    cin >> Month;
+    return Month;
+}
+
+short ReadYear()
+{
+    short Year;
+    cout << "Please enter a Year? ";
+    cin >> Year;
+    return Year;
+}
+
+stDate ReadFullDate()
+{
+    stDate Date;
+
+    Date.Day = ReadDay();
+    Date.Month = ReadMonth();
+    Date.Year = ReadYear();
+
+    return Date;
+}
+
+int main()
+{
+    stDate Date1 = ReadFullDate();
+
+    Date1 = IncreaseDateByOneDay(Date1);
+
+    cout << "\nDate after adding one day is: "
+        << Date1.Day << "/" << Date1.Month << "/" << Date1.Year;
+
+    system("pause>0");
+    return 0;
+}
