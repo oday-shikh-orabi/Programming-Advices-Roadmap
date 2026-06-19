b3bdde9c255a103f15d1f4ff12975940941a4162
oday <oday.sheikh.dev@gmail.com>
2026-06-19 16:59:22 +0300

    Manage Users Screen Added

--- 11 - OOP as it Should Be (Applications)/Project 3 Bank System/Project 3 Bank System/clsMainScreen.h
+++ 11 - OOP as it Should Be (Applications)/Project 3 Bank System/Project 3 Bank System/clsMainScreen.h
@@ -8,6 +8,7 @@
 #include "clsUpdateClientScreen.h"
 #include "clsFindClientScreen.h"
 #include "clsTransactionsScreen.h"
+#include "clsManageUsersScreen.h"
 
 using namespace std;
 
@@ -84,8 +85,8 @@ private:
 
     static void _ShowManageUsersMenue()
     {
-        cout << "\nUsers Menue Will be here...\n";
-
+        // cout << "\nUsers Menue Will be here...\n";
+        clsManageUsersScreen::ShowManageUsersMenue();
     }
 
     static void _ShowEndScreen()
