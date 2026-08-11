23ceadaf14c3d50922cc9f9e969bf6d9be593a4a
oday <oday.sheikh.dev@gmail.com>
2026-08-11 14:43:01 +0300

    Add (Dynamic Array) Extension

--- 13 - Algorithms & Problem Solving Level 5/Project 2 My Queue/Project 2 My Queue/Project 2 My Queue.cpp
+++ 13 - Algorithms & Problem Solving Level 5/Project 2 My Queue/Project 2 My Queue/Project 2 My Queue.cpp
@@ -30,6 +30,39 @@ int main()
     cout << "\n\nQueue after pop() : \n";
     MyQueue.Print();
 
+    //Extension #1
+    cout << "\n\n Item(2) : " << MyQueue.GetItem(2);
+
+    //Extension #2
+    MyQueue.Reverse();
+    cout << "\n\nQueue after reverse() : \n";
+    MyQueue.Print();
+
+    //Extension #3
+    MyQueue.UpdateItem(2, 600);
+    cout << "\n\nQueue after updating Item(2) to 600 : \n";
+    MyQueue.Print();
+
+    //Extension #4
+    MyQueue.InsertAfter(2, 800);
+    cout << "\n\nQueue after Inserting 800 after Item(2) : \n";
+    MyQueue.Print();
+
+
+    //Extension #5
+    MyQueue.InsertAtFront(1000);
+    cout << "\n\nQueue after Inserting 1000 at front: \n";
+    MyQueue.Print();
+
+    //Extension #6
+    MyQueue.InsertAtBack(2000);
+    cout << "\n\nQueue after Inserting 2000 at back: \n";
+    MyQueue.Print();
+
+    //Extension #7
+    MyQueue.Clear();
+    cout << "\n\nQueue after Clear(): \n";
+    MyQueue.Print();
 
 
 
